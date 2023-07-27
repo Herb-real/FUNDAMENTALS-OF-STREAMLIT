62f66eeb56a54c50ac8d5d495445e3f3a6a4218e
Herbert Djanie <hdjaniejnr@gmail.com>
2023-07-27 09:07:10 +0000

    data updated

--- src/app.py
+++ src/app.py
@@ -16,12 +16,12 @@ petal_width = st.number_input("Enter petal width")
 # prediction executed
 if st.button("Predict"):
     # Dataframe creation
-    df=pd.DataFrame(
+    df = pd.DataFrame(
         {
          "sepal_height":[sepal_height],  "sepal_width":[sepal_width], "petal_height":[petal_height], "petal_width":[petal_width], 
             
         }   
     )
-   print(f"[Info] Input data as dataframe:\n{df.to_markdown()}") 
+    print(f"[Info] Input data as dataframe:\n{df.to_markdown()}") 
     
-   st.text(f"The iris has been classified as : '{''}'.")
+    st.text(f"The iris has been classified as : '{''}'.")
